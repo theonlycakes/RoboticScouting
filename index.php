5297cf3416b0f526e5b4168928b97cee9b0fe69a
Peter Spychalsky <23511567+theonlycakes@users.noreply.github.com>
2020-03-07 16:54:47 -0500

    Added online pages

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Home</title>
+</head>
+
+<body>
+	<a href="/Robotics Scouting PageOnline.html">Scouting Page</a><br>
+	<a href="/Pit ScoutingOnline.html">Pit Scouting Page</a>
+</body>
+</html>
--- upload.php
+++ upload.php
@@ -1,36 +1,15 @@
-<!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-?>
+<?php 
+	$myfile = fopen("uploads\\" .$_POST["name"], "w") or die("Unable to open file!");
+	$txt = $_POST["data"];
+	fwrite($myfile, $txt);
+	fclose($myfile);
+	echo "<script>location.href='Robotics Scouting PageOnline.html';</script>";
+	?>
+
+</body>
+</html>
+
 <body>
 </body>
 </html>
